cfa0fd0efa68b6aeec5a77c0baa4894e7e51a55a
dpatel <darshan.patel@mediafuturesgroup.com>
2024-02-16 11:31:06 -0500

    ML Google Engineer Course

---
+++ AQ_testscript2.mjs
@@ -0,0 +1,15 @@
+
+//import * as d3 from 'https://d3js.org/d3.v7.min.js';
+//const d3 = require('d3')
+
+import * as d3 from 'd3'
+
+//import { range } from 'd3-array'
+//console.log(range(10))
+
+let index =
+d3.range(
+    1,15
+)
+
+console.log(index)
